69a4828380a5b3cb77861397ed894099d07b90fa
xmatute- <xmatute-@student.42urduliz.com>
2025-08-13 20:37:15 +0200

    basic main

--- ready-set-boole/src/main.rs
+++ ready-set-boole/src/main.rs
@@ -6,133 +6,75 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 14:18:55 by xmatute-          #+#    #+#             */
-/*   Updated: 2025/08/10 18:45:18 by xmatute-         ###   ########.fr       */
+/*   Updated: 2025/08/13 20:36:41 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-use ready_set_boole::ex03::eval_formula::eval_formula;
+fn debug_format(n: u32) -> String {
+    format!("{0:#032b} {0}", n)
+}
 
 fn main() {
     println!("Ready, Set, Boole!");
     
-    println!("{}", eval_formula("11&")); // Example formula in RPN
-    println!("{}", eval_formula("1A&")); // Example formula in RPN
-}
+    let a = 40;
+    let b = 2;
+    let sum = ready_set_boole::ex00::adder::adder(a, b);
 
+    println!("The sum of {} and {} is: {}", a, b, sum);
 
+    let c = 10;
+    let mult = ready_set_boole::ex01::multiplier::multiplier(sum, c);
 
-// fn main() {
-//     println!("Ready, Set, Boole!");
-//     // let a = 3;
-//     // let b = 5;
-//     // let sum = adder(a, b);
-//     // println!("The sum of {} and {} is {}", a, b, sum);
-//     // let formula = "11&"; // Example formula in RPN
-//     // let result = eval_formula(formula);
-//     // println!("The result of the formula '{}' is {}", formula, result);
-//     // let truth_table_formula = "A B &"; // Example formula for truth table
-//     // let f = truth_table_formula.parse::<extra::formula::Formula>()
-//     //     .expect("Invalid formula");
-//     // println!("Truth table for formula '{}':", truth_table_formula.parse::<extra::formula::Formula>().unwrap());
-//     // let s = f.substitute(&HashMap::from([
-//     //     ('A', Formula::True),
-//     // ]));
-
-//     // truth_table::print_truth_table("A B C & |"); // Example formula for truth table with three variables
-//     // println!("{}", f.to_rpn());
-
-//     // println!("{}", negation_normal_form("AB&!")); // Output: A!B!|
-//     // println!("{}", negation_normal_form("AB|!")); // Output: A!B!&
-//     // println!("{}", negation_normal_form("AB>"));  // Output: A!B|
-//     // println!("{}", negation_normal_form("AB="));  // Output: AB&A!B!&|
-//     // println!("{}", negation_normal_form("AB|C&!")); // Output: A!B!&C!|
-        
-//     // println!("{}", conjunctive_normal_form("AB&!")); // Output: A!B!|
-//     // println!("{}", conjunctive_normal_form("AB|!")); // Output: A!B!&
-//     // println!("{}", conjunctive_normal_form("AB|C&")); // Output: AB|C&
-//     // println!("{}", conjunctive_normal_form("AB|C|D|")); // Output: ABCD|||
-//     // println!("{}", conjunctive_normal_form("AB&C&D&")); // Output: ABCD&&&
-//     // println!("{}", conjunctive_normal_form("AB&!C!|")); // Output: A!B!C!||
-//     // println!("{}", conjunctive_normal_form("AB|!C!&")); // Output: A!B!C!&&
-
-//     // println!("{}", sat("AB|")); // Output: true
-//     // println!("{}", sat("AB&")); // Output: true
-//     // println!("{}", sat("AA!&")); // Output: false
-//     // println!("{}", sat("AA^")); // Output: false
-
-//     // let set = vec![1, 2, 3];
-//     // let result = powerset(set);
-
-//     // for subset in result {
-//     //     println!("{:?}", subset);
-//     // }
-
-//     // let sets = vec![vec![0, 1, 2], vec![0, 3, 4]];
-//     // let result = eval_set("AB&", sets);
-//     // println!("{:?}", result); // Output: [0]
-
-//     // let sets = vec![vec![0, 1, 2], vec![3, 4, 5]];
-//     // let result = eval_set("AB|", sets);
-//     // println!("{:?}", result); // Output: [0, 1, 2, 3, 4, 5]
-
-//     // let sets = vec![vec![0, 1, 2]];
-//     // let result = eval_set("A!", sets);
-//     // println!("{:?}", result); // Output: []
-
-//     // let x = 42;
-//     // let y = 84;
-
-//     // let result = map(x, y);
-//     // println!("Mapping ({}, {}) -> {}", x, y, result);
-
-//     // let x = 0;
-//     // let y = 0;
-//     // println!("Mapping ({}, {}) -> {}", x, y, map(x, y));
-
-//     // let x = u16::MAX;
-//     // let y = u16::MAX;
-//     // println!("Mapping ({}, {}) -> {}", x, y, map(x, y));
-
-//     // // Example usage
-//     // let x = 42;
-//     // let y = 84;
-
-//     // // Map (x, y) to a value in [0, 1]
-//     // let mapped_value = crate::ex10::curve::map(x, y);
-//     // println!("Mapped ({}, {}) -> {}", x, y, mapped_value);
-
-//     // // Reverse the mapping
-//     // let (decoded_x, decoded_y) = reverse_map(mapped_value);
-//     // println!("Reverse mapped {} -> ({}, {})", mapped_value, decoded_x, decoded_y);
-
-//     // // Test with edge cases
-//     // let mapped_value = crate::ex10::curve::map(0, 0);
-//     // println!("Reverse mapped {} -> {:?}", mapped_value, reverse_map(mapped_value));
-
-//     // let mapped_value = crate::ex10::curve::map(u16::MAX, u16::MAX);
-//     // println!("Reverse mapped {} -> {:?}", mapped_value, reverse_map(mapped_value));
+    println!("The product of {} and {} is: {}", sum, c, mult);
     
-// }
+    let gray = ready_set_boole::ex02::gray_code::gray_code(mult);
 
+    println!("The Gray code of {} is: {}", debug_format(mult), debug_format(gray));
 
-// #[test]
-// fn test_adder() {
-//     assert_eq!(adder(0, 0), 0);
-//     assert_eq!(adder(1, 1), 2);
-//     assert_eq!(adder(2, 3), 5);
-//     assert_eq!(adder(10, 20), 30);
-//     assert_eq!(adder(100, 200), 300);
-//     assert_eq!(adder(123456789, 987654321), 1111111110);
-// }
+    let formula = "0!1&10|^!";
+    let result = ready_set_boole::ex03::eval_formula::eval_formula(formula);
 
-// Symbol Mathematical
-// equivalent
-// Description
-// 0 ⊥ false
-// 1 ⊤ true
-// ! ¬ Negation
-// & ∧ Conjunction
-// | ∨ Disjunction
-// ˆ ⊕ Exclusive disjunction
-// > ⇒ Material condition
-// = ⇔ Logical equivalence
+    println!("The evaluation of the formula '{}' is: {}", formula, result);
+
+    let formula = "A!B&CD|^!";
+    println!("The truth table for the formula '{}' is:", formula);
+
+    ready_set_boole::ex04::truth_table::print_truth_table(formula);
+
+    let nnf = ready_set_boole::ex05::nnf::negation_normal_form(formula);
+    println!("The NNF of the formula '{}' is: {}", formula, nnf);
+    ready_set_boole::ex04::truth_table::print_truth_table(&nnf);
+    
+    let cnf = ready_set_boole::ex06::cnf::conjunctive_normal_form(&nnf);
+    println!("The CNF of the formula '{}' is: {}", nnf, cnf);
+    ready_set_boole::ex04::truth_table::print_truth_table(&cnf);
+
+    if ready_set_boole::ex07::sat::sat(&formula) {
+        println!("The formula '{}' is satisfiable.", formula);
+    } else {
+        println!("The formula '{}' is not satisfiable.", formula);
+    }
+
+    let set : Vec<i32> = vec![1, 2, 3];
+    let power_set = ready_set_boole::ex08::powerset::powerset(set.clone());
+    println!("The power set of {:?} is: {:?}", set, power_set);
+
+    let sets : Vec<Vec<i32>> = vec![
+        vec![1, 2],
+        vec![2, 3],
+        vec![1, 3],
+        vec![4, 5],
+    ];
+
+    let result_set = ready_set_boole::ex09::set_evaluation::eval_set(formula, sets.clone());
+    println!("The evaluation of the set formula 'A&B|C' with sets {:?} is: {:?}", sets, result_set);
+
+    let x: u16 = 5;
+    let y: u16 = 10;
+
+    let mapped_value = ready_set_boole::ex10::curve::map(x, y);
+    println!("The mapped value for coordinates ({}, {}) is: {}", x, y, mapped_value);
+    let coordinates = ready_set_boole::ex11::inverse_function::reverse_map(mapped_value);
+    println!("The coordinates for the mapped value {} are: ({}, {})", mapped_value, coordinates.0, coordinates.1);
+}
